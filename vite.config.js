35e6f9d269362413e229e006d918745f27d5edc5
Aiyana Scarborough <aiyanascar@Aiyanas-Laptop.attlocal.net>
2024-08-12 13:05:34 -0400

    fix index.html

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/bug-free-dollop/',  // Set this to your repository name
+  base: '/bug-free-dollop/',  
   plugins: [react()],
 });
 
